20febcc63a1ff3c83152b0fe7a78fd34c412c3b0
karefined-eng <ka.refined@gmail.com>
2026-09-10 13:35:15 +0000

    feat: add structured data component and centralized link definitions

--- lib/links.ts
+++
@@ -1,9 +0,0 @@
-export const GITHUB_PROFILE_URL = "https://github.com/karefined-eng";
-export const GITHUB_URL = "https://github.com/karefined-eng/eleviewer";
-export const RELEASES_URL = "https://github.com/karefined-eng/eleviewer/releases";
-export const LATEST_RELEASE_VERSION = "1.4.0"
-export const LATEST_RELEASE_SHA256 = "5e51f50c7701d427d2f27e49a7c29a180f3816ebbe394dc745f79f2e85a901fe"
-export const DOWNLOAD_URL = "https://github.com/karefined-eng/eleviewer/releases/latest/download/EleViewer.exe";
-export const ISSUES_URL = "https://github.com/karefined-eng/eleviewer/issues";
-export const LICENSE_URL = "https://github.com/karefined-eng/eleviewer/blob/main/LICENSE";
-
